b528a090c8e9faa75dac6ba6cf62d3b843ab1b9a
Veronica <almeida.vrc@gmail.com>
2023-05-15 11:20:24 -0300

    Add challenges from 1 to 11

---
+++ src/desafios/modulo01/_04_ExibeMaiorNumero.java
@@ -0,0 +1,22 @@
+package desafios.modulo01;
+
+import java.util.Scanner;
+
+public class _04_ExibeMaiorNumero {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int numeroUm;
+        int numeroDois;
+
+        System.out.println("Digite dois números inteiros:");
+
+        numeroUm = leitor.nextInt();
+        numeroDois = leitor.nextInt();
+
+        if (numeroUm >= numeroDois) {
+            System.out.println("O maior número digitado foi " + numeroUm);
+        } else {
+            System.out.println("O maior número digitado foi " + numeroDois);
+        }
+    }
+}
---
+++ src/desafios/modulo01/_08_VerificaSePositivoNegativoOuNulo.java
@@ -0,0 +1,23 @@
+package desafios.modulo01;
+
+import java.util.Scanner;
+
+public class _08_VerificaSePositivoNegativoOuNulo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int numero;
+
+
+        System.out.println("Informe um número:");
+
+        numero = leitor.nextInt();
+
+        if (numero < 0) {
+            System.out.println("Negativo");
+        } else if (numero > 0) {
+            System.out.println("Positivo");
+        } else {
+            System.out.println("Nulo");
+        }
+    }
+}
